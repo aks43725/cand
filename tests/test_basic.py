47e8449f7293c23fa31a7d87205ada10874a1ffd
Ching-Wei Cheng <aks43725@gmail.com>
2019-04-22 18:00:00 -0400

    Add files via upload

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,24 +5,31 @@ import scipy as sp
 import pandas as pd
 import cand
 
-riboflavin = pd.read_csv('data/riboflavin.csv', index_col=0).T
-y = riboflavin['q_RIBFLV']
-X = riboflavin.drop(['q_RIBFLV'], axis=1)
+ribflv = pd.read_csv('data/riboflavin.csv', index_col=0).T
+y = ribflv['q_RIBFLV']
+X = ribflv.drop(['q_RIBFLV'], axis=1)
 n, d = X.shape
 
-np.random.seed(2549)
 
-if __name__ == '__main__':
-  
-  # Implementing the GA
-  mobj_GA = cand.GA(X, y)
-  mobj_GA.fit()
-  
-  # Implementing the SA to search for good models of sizes appeared in the last GA generation
-  count = np.bincount(mobj_GA.generations['model_size'][-1])
-  SA_sizes = np.nonzero(count)[0]
-  mobj_SA = cand.SA(X, y)
-  mobj_SA.fit(SA_sizes)
-  
-  # Implementing the RP:
-  mobj_RP = cand.union(X, y).fit()
+#if __name__ == '__main__':
+
+# The GA
+for seed in range(1, 100):
+  np.random.seed(111)
+  models_GA = cand.GA(X, y)
+  models_GA.fit()
+  print(models_GA.fitness)
+  print(models_GA.models.sum(axis=1))
+  if models_GA.models.sum(axis=1)[0] == 1:
+    break
+
+# The SA, with model sizes appeared in the last GA generation
+count = np.bincount(mobj_GA.generations['model_size'][-1])
+SA_sizes = np.nonzero(count)[0]
+models_SA = cand.SA(X, y)
+models_SA.fit(SA_sizes, verbose=True)
+print(models_SA.fitness)
+print(models_SA.models.sum(axis=1))
+
+# The RP:
+models_RP = cand.union(X, y).fit()
