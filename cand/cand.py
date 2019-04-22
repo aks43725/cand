8b19661af41870ccf0e805bd2c1a6b4ddd6b3602
Ching-Wei Cheng <aks43725@gmail.com>
2019-04-22 18:00:21 -0400

    Add files via upload

--- cand/cand.py
+++ cand/cand.py
@@ -26,7 +26,8 @@ class candidate_models:
       self.varimp = None
     else:
       self.calculate_varimp()
-      self.varimp[self.varimp == 0.0] = min(1e-7, np.min(self.varimp[self.varimp > 0.0]))
+      if any(self.varimp == 0.0):
+        self.varimp[self.varimp == 0.0] = min(1e-7, np.min(self.varimp[self.varimp > 0.0]))
   
   def calculate_varimp(self):
     if self.varimp_type == 'marcor':
@@ -38,40 +39,40 @@ class candidate_models:
       else:
         self.varimp = np.dot(XX.T, np.linalg.inv(np.dot(XX, XX.T))).dot(self.y)[1:]
     elif self.varimp_type.upper() == 'NR17':
+      # A variable importance method proposed by Nevo and Ritov (2017, JMLR)
       # Lasso
-      l1_fit = linear_model.LassoCV(n_jobs=-1).fit(self.X, self.y)
+      l1_fit = linear_model.LassoCV(cv=5, n_jobs=-1).fit(self.X, self.y)
       l1_coef = l1_fit.coef_
       l1_model = np.where(np.abs(l1_coef) > 0.0)[0]
       # Elastic net
-      en_fit = linear_model.ElasticNetCV(n_jobs=-1).fit(self.X, self.y)
+      en_fit = linear_model.ElasticNetCV(cv=5, n_jobs=-1).fit(self.X, self.y)
       en_coef = en_fit.coef_
       en_model = np.where(np.abs(en_coef) > 0.0)[0]
       #
-      lasso_only = list(set(l1_model) - set(en_model))
       en_only = np.array(list(set(en_model) - set(l1_model)))
       penalties = np.ones(self.X.shape[1])
       delta = np.linspace(0.0, 0.1, 51)
-      sims_coef_las_step2 = np.zeros((delta.size, self.X.shape[1]))
       if en_only.size > 0:
+        coef_l1_plus = np.zeros((delta.size, self.X.shape[1]))
         for i in range(len(delta) - 1):
           penalties[en_only] = delta[i]
-        
-        sims_coef_las_step2[i, :] = glmnet.ElasticNet(alpha=1.0).fit(self.X, self.y, relative_penalties=penalties).coef_
-      
-      sims_coef_las_step2[-1, :] = l1_coef
-      self.varimp = np.zeros(self.X.shape[1])
-      if en_only.size > 0:
-        for i in en_only:
-          coef = sims_coef_las_step2[:, i]
-          ij = np.max(np.where(coef != 0.0)[0]) if any(coef != 0.0) else None
-          self.varimp[i] = delta[ij] / 2.0 if ij is not None else 0.0
-      
-      if l1_model.size > 0:
-        for i in l1_model:
-          coef = sims_coef_las_step2[:, i]
-          ij = np.max(np.where(coef == 0.0)[0]) if any(coef == 0.0) else None
-          self.varimp[i] = 1.0 - delta[ij] / 2.0 if ij is not None else 1.0
-      
+          coef_l1_plus[i, :] = glmnet.ElasticNet(alpha=1.0).fit(self.X, self.y, relative_penalties=penalties).coef_
+        coef_l1_plus[-1, :] = l1_coef
+        self.varimp = np.zeros(self.X.shape[1])
+        for j in range(self.X.shape[1]):
+          if j in l1_model:
+            ij = np.argmax(coef_l1_plus[:, j] == 0.0) if any(coef_l1_plus[:, j] == 0.0) else None
+            self.varimp[j] = 1.0 - delta[ij] / 2.0 if ij is not None else 0.0
+          else:
+            if j in en_only:
+              ij = np.argmax(coef_l1_plus[:, j] != 0.0) if any(coef_l1_plus[:, j] != 0.0) else None
+              self.varimp[j] = delta[ij] / 2.0 if ij is not None else 0.0
+            else:
+              self.varimp[j] = 0.0
+      else:
+        ij = np.argmax(l1_coef == 0.0) if any(l1_coef == 0.0) else None
+        for j in range(X.shape[1]):
+          self.varimp[j] = 1.0 - delta[ij] / 2.0 if j in l1_model else 0.0
     else:
       self.varimp = np.ones(self.d)
 
@@ -81,15 +82,12 @@ class GA(candidate_models):
   '''
   A genetic algorithm for high-quality model search
   '''
-  def __init__(self, X, y, evaluator='PLIC', varimp_type='marcor', popSize=0, selection='icmw', mutation_type=None, mutation_rate=None, ggap=15, maxGen=100):
+  def __init__(self, X, y, evaluator='PLIC', varimp_type='marcor', popSize=0, selection='proportional', mutation_type='varimp', mutation_rate=None, ggap=15, maxGen=100):
     candidate_models.__init__(self, X, y, evaluator, varimp_type)
     if popSize <= 0:
       popSize = int(4 * np.ceil(1 + np.log(-X.shape[1] / np.log(0.9999)) / np.log(2)))
     self.popSize, self.selection = popSize, selection
-    if mutation_type is None:
-      self.mutation_type = 'uniform'
-    else:
-      self.mutation_type = mutation_type
+    self.mutation_type = 'uniform' if mutation_type is not 'varimp' else mutation_type
     
     if mutation_rate is None:
       self.mutation_rate = 1.0 / X.shape[1]
@@ -108,41 +106,40 @@ class GA(candidate_models):
   def get_fitness(self, models, n_jobs=-1):
     # Negative GIC as fitness
     fitness = -GIC(self.X, self.y, models, self.evaluator, n_jobs)
-    
     # Update 'worst_fitness'
     self.worst_fitness = np.nanmin(np.append(self.worst_fitness, fitness))
     return np.where(pd.isnull(fitness), self.worst_fitness, fitness)
   
-  def uniform_xover(self, models, prob_select=None):
+  def uniform_xover(self, models, prob_select):
     if prob_select.sum() is not 1.0:
       prob_select = prob_select / prob_select.sum()
-    
     m = models[np.random.choice(range(models.shape[0]), size=2, replace=True, p=prob_select), :]
-    if any(m[0,:] != m[1,:]):
-      idx = np.where(m[0,:] != m[1,:])[0]
+    if any(m[0, :] != m[1, :]):
+      idx = np.where(m[0, :] != m[1, :])[0]
       idxx = np.random.binomial(1, 0.5, size=idx.size)
       if idxx.sum() > 0:
         m[0, idx[idxx == 1]], m[1, idx[idxx == 1]] = m[1, idx[idxx == 1]], m[0, idx[idxx == 1]]
-    
     # Mutation
-    for k in range(m.shape[0]):
-      prob_mutate = np.repeat(self.mutation_rate, self.d)
-      if (self.mutation_type == 'varimp') and (np.min(self.varimp) != np.max(self.varimp)):
-        idx0 = np.where(m[k, :] == 0)[0]
-        prob_mutate[idx0] *= idx0.size * self.varimp[idx0] / np.sum(self.varimp[idx0])
-        idx1 = np.where(m[k, :] == 1)[0]
-        prob_mutate[idx1] *= idx1.size / self.varimp[idx1] / np.sum(1.0 / self.varimp[idx1])
-
-      idx_mutate = np.where(np.random.random(m[k, :].size) < self.mutation_rate)[0]
-      if idx_mutate.size > 0:
-        m[k, idx_mutate] = 1 - m[k, idx_mutate]
-    
+    if self.mutation_rate > 0.0:
+      for k in range(m.shape[0]):
+        prob_mutate = np.repeat(self.mutation_rate, self.d)
+        if (self.mutation_type == 'varimp') and (self.varimp.min() != self.varimp.max()):
+          idx0 = np.where(m[k, :] == 0)[0]
+          if idx0.size > 0:
+            pp = self.varimp[idx0]
+            prob_mutate[idx0] *= idx0.size * pp / pp.sum()
+          idx1 = np.where(m[k, :] != 0)[0]
+          if idx1.size > 0:
+            pp = np.where(np.isinf(1.0 / self.varimp[idx1]), 0.0, 1.0 / self.varimp[idx1])
+            prob_mutate[idx1] *= idx1.size * pp / pp.sum()
+        idx_mutate = np.where(np.random.random(self.d) < prob_mutate)[0]
+        if idx_mutate.size > 0:
+          m[k, idx_mutate] = 1 - m[k, idx_mutate]
     return m
   
   def fit(self, init_models='RP', model_history=False, seed=None, verbose=False, n_jobs=-1):
     if seed is not None:
       np.random.seed(seed)
-    
     # Generate initial population, fitness evaluation, and sort the models by fitness
     if isinstance(init_models, np.ndarray):
       self.models = 1 * (init_models != 0)
@@ -174,7 +171,7 @@ class GA(candidate_models):
     it = 0
     while not converge and (it <= self.maxGen):
       # Check whether all current models are infeasible
-      if np.unique(self.fitness).size == 1:
+      if np.isnan(self.worst_fitness):
         raise RuntimeError('All models in generation {} are infeasible'.format(it))
       it += 1
       if verbose:
@@ -183,10 +180,10 @@ class GA(candidate_models):
       old_models = self.models.copy()
       # Elitism selection: keep the best model
       self.models = old_models[0, :]
-      if len(set(self.fitness)) == 1:
+      if np.unique(self.fitness).size == 1:
         # If fitness values are all the same, it doesn't matter using which model weighting
         prob_select = np.ones(self.popSize) / self.popSize
-      elif self.selection == 'icmw':
+      elif self.selection == 'proportional':
         # Proportionate selection via GIC-based model weighting
         prob_select = lsIC(self.X, self.y, old_models, self.evaluator, ic=-self.fitness, n_jobs=n_jobs)
       else:
@@ -194,8 +191,8 @@ class GA(candidate_models):
         fitness_rank = scipy.stats.rankdata(self.fitness, method='ordinal')
         prob_select = 2.0 * fitness_rank.astype('float') / fitness_rank.size / (fitness_rank.size + 1.0)
       # Uniform crossover and mutation
-      children = np.vstack([self.uniform_xover(old_models, prob_select) for _ in range(self.popSize // 2)])
-      #children = np.vstack([self.uniform_xover(old_models, prob_select) for _ in range(self.popSize - 1)])
+      #children = np.vstack([self.uniform_xover(old_models, prob_select) for _ in range(self.popSize // 2)])
+      children = np.vstack([self.uniform_xover(old_models, prob_select) for _ in range(self.popSize - 1)])
       self.models = np.vstack((self.models, children))
       
       # Fitness evaluation and sort the models by fitness
@@ -245,7 +242,7 @@ class SA(candidate_models):
   '''
   Simulated annealing algorithm of
     Daniel Nevo, Ya'acov Ritov (2017)
-    Identifying a Minimal Class of Models for High-dimensional Data
+    "Identifying a Minimal Class of Models for High-dimensional Data"
     Journal of Machine Learning Research, 18(24):1-29
   '''
   def __init__(self, X, y, evaluator='PLIC', varimp_type='NR17', popSize=100, max_iter=100):
@@ -259,12 +256,10 @@ class SA(candidate_models):
     if model_size > self.varimp.size:
       warnings.warn('[model_size] should be at most {:d}'.format(self.X.shape[1]))
       return 
-    
     model = np.zeros(self.varimp.size).astype('int')
     taken = np.where(self.varimp > np.sort(self.varimp)[-model_size])[0]
     if taken.size > 0:
       model[taken] = 1
-    
     nfree = model_size - taken.size
     idx = np.where(self.varimp == np.sort(self.varimp)[-model_size])[0]
     if idx.size > 0:
@@ -277,40 +272,35 @@ class SA(candidate_models):
     Iteration for fixed temperature (BoltzmanT), a model is suggested and then a decision is made
     '''
     new_model = old_model.copy()
-    # Choose a variable to be kicked out
-    prob_old_out = np.zeros(self.X.shape[1])
-    prob_old_out[old_model == 1] = 1.0 / self.varimp[old_model == 1]
-    prob_old_out = np.where(pd.isnull(prob_old_out), 0.0, prob_old_out)
-    if np.all(prob_old_out == 0.0):
-      prob_old_out = np.ones(prob_old_out.size) / prob_old_out.size
-    else:
-      prob_old_out = prob_old_out / prob_old_out.sum()
-    var_out = np.random.choice(range(self.X.shape[1]), 1, p=prob_old_out)
-    # Choose a variable to be added in
-    prob_old_in = np.zeros(self.X.shape[1])
-    prob_old_in[old_model == 0] = self.varimp[old_model == 0]
-    prob_old_in = np.where(pd.isnull(prob_old_in), 0.0, prob_old_in)
-    if np.all(prob_old_in == 0.0):
-      prob_old_in = 1.0 / prob_old_out.size
-    else:
-      prob_old_in = prob_old_in / prob_old_in.sum()
-    prob_old_in = prob_old_in / prob_old_in.sum()
-    var_in = np.random.choice(range(self.X.shape[1]), 1, p=prob_old_in)
-    new_model[var_out], new_model[var_in] = 0, 1
-    #
+    def var_in_out(model, varimp, type_):
+      d = model.size
+      k = np.sum(model != 0.0)
+      idx = np.where(model == 0)[0] if type_ == "in" else np.where(model != 0)[0]
+      if type_ == 'in':
+        idx = np.where(model == 0)[0]
+        prob = np.ones(d - k) / (d - k) if all(varimp[idx] == 0.0) else varimp[idx] / varimp[idx].sum()
+      else:
+        idx = np.where(model != 0.0)[0]
+        prob = 1.0 / varimp[idx]
+        prob = np.where(np.isinf(prob), 0.0, prob)
+        prob /= prob.sum()
+      variable = np.random.choice(idx, 1, p=prob)
+      return idx, prob, variable
+    # Probabilities to transition to a new model
+    old_out = var_in_out(old_model, self.varimp, 'out')
+    old_in = var_in_out(old_model, self.varimp, 'in')
+    new_model[old_out[2]], new_model[old_in[2]] = 0, 1
+    prob_old2new = old_out[1][old_out[0] == old_out[2]] * old_in[1][old_in[0] == old_in[2]]
     # Probabilities from the new model back to the old model
-    prob_new_in = np.zeros(self.X.shape[1])
-    prob_new_in[new_model == 0] = self.varimp[new_model == 0]
-    prob_new_in = prob_new_in / prob_new_in.sum()
-    prob_new_out = np.zeros(self.X.shape[1])
-    prob_new_out[new_model == 1] = 1.0 / self.varimp[new_model == 1]
-    prob_new_out = prob_new_out / prob_new_out.sum()
+    new_in = var_in_out(new_model, self.varimp, 'in')
+    new_out = var_in_out(new_model, self.varimp, 'out')
+    prob_new2old = new_in[1][new_in[0] == old_out[2]] * new_out[1][new_out[0] == old_in[2]]
     #
     # GIC values of the old and new models
     ic = GIC(self.X, self.y, np.array([old_model, new_model]), self.evaluator, n_jobs)
     #
     # Whether to accept the new model
-    decision_prob = np.exp((ic[1] - ic[0]) / BoltzmanT) * prob_new_out[var_in] * prob_new_in[var_out] / prob_old_out[var_out] / prob_old_in[var_in]
+    decision_prob = np.exp((ic[0] - ic[1]) / 2.0 / BoltzmanT) * prob_new2old / prob_old2new
     decision_prob = min(1.0, decision_prob)
     decision = 1 if np.random.uniform() < decision_prob else 0
     return {'decision': decision, 'model': np.array([old_model, new_model]), 'ic': ic}
@@ -318,10 +308,9 @@ class SA(candidate_models):
   def run_constant_temperature(self, init_model, BoltzmanT, models, ic, n_jobs=-1):
     '''
     init_model: Initial model
-    current_ic: GIC of the current best models
-    current_top_models: Current best models
     BoltzmanT: Temperature sequence
-    max_init: Number of iterations per temperature
+    models: Current best models
+    ic: GIC of the current best models
     '''
     if models.shape[0] == 0:
       models = np.array(init_model)
@@ -337,46 +326,40 @@ class SA(candidate_models):
         else:
           models[-1, :] = met_res['model'][1, :]
           ic[-1] = met_res['ic'][1]
-        
-        idx = np.argsort(ic)
-        models, ic = models[idx, :], ic[idx]
+        if ic.size > 1:
+          idx = np.argsort(ic)[:min(self.popSize, ic.size)]
+          models, ic = models[idx, :], ic[idx]
 
     return models, ic, init_model
-
+  
   def fit(self, model_sizes, T_seq=None, verbose=False, n_jobs=-1):
     self.models = np.empty((0, self.X.shape[1]), 'int')
     self.ic = np.array([])
     if T_seq is None:
       T_seq = 10.0 * 0.7**np.linspace(1, 20, 20)
-    for k in model_sizes:
+    for k in tqdm.tqdm(model_sizes) if verbose else model_sizes:
       if verbose:
         print('SA with model size {:d}'.format(k))
-      
-      if k > self.varimp.size:
+      if k > min(self.varimp.size, self.X.shape[0] - 2):
         warnings('Cannot process model size {:d}'.format(k))
         continue
-      
-      #init_model = self.generate_init_model(k)
-      init_model = np.zeros(self.X.shape[1]).astype('int')
-      init_model[:k] = 1
-      #
-      # Simulated annealing algorithm
-      old_model = init_model.copy()
-      models_k = np.empty((0, self.X.shape[1]), 'int')
-      ic_k = np.array([])
-      for t in tqdm.tqdm(T_seq) if verbose else T_seq:
-        models_k, ic_k, old_model = self.run_constant_temperature(old_model, t, models_k, ic_k, n_jobs)
+      elif k == 0:
+        models_k = np.zeros(self.varimp.size)
+        ic_k = GIC(self.X, self.y, models_k, self.evaluator, n_jobs)
+      else:
+        init_model = self.generate_init_model(k)
+        #
+        # Simulated annealing algorithm
+        old_model = init_model.copy()
+        models_k = np.empty((0, self.X.shape[1]), 'int')
+        ic_k = np.array([])
+        for t in tqdm.tqdm(T_seq) if verbose else T_seq:
+          models_k, ic_k, old_model = self.run_constant_temperature(old_model, t, models_k, ic_k, n_jobs)
       
       self.models = np.vstack((self.models, models_k))
       self.ic = np.append(self.ic, ic_k)
     
     # Remove duplicate models and sort the models by the their GIC values
-    idx = np.argsort(self.ic)
-    if self.ic.size > self.popSize:
-      self.models, self.ic = self.models[idx[:self.popSize], :], self.ic[idx[:self.popSize]]
-    else:
-      self.models, self.ic = self.models[idx, :], self.ic[idx]
-    
     self.models, idx = np.unique(self.models, return_index=True, axis=0)
     self.ic = self.ic[idx]
     idx = np.argsort(self.ic)
